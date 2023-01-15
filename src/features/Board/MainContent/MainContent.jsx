88dd4f2ff5bf94eea565c9213b8ca0285394d215
trizotti <gootrizotti@gmail.com>
2023-01-15 17:40:46 -0300

    Add shufled message

--- src/features/Board/MainContent/MainContent.jsx
+++ src/features/Board/MainContent/MainContent.jsx
@@ -5,10 +5,15 @@ function MainContent() {
   const { selectedCard } = useDeck()
   return (
     <main className={styles.pageContent}>
-      {selectedCard && (
+      {selectedCard ? (
         <div className={styles.cardSymbols}>
           <h2>{selectedCard.title}</h2>
           <div>{selectedCard.content}</div>
+        </div>
+      ) : (
+        <div className={styles.cardSymbols}>
+          <h2>O deck está embaralhado</h2>
+          <div>Selecione uma carta para continuar</div>
         </div>
       )}
     </main>
