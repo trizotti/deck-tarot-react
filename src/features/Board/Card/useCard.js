c2cc53fdf72f95e191bcb551635e87afd8dde85b
trizotti <gootrizotti@gmail.com>
2023-01-15 22:28:32 -0300

    Improve performance with image preload

--- src/features/Board/Card/useCard.js
+++ src/features/Board/Card/useCard.js
@@ -1,12 +1,23 @@
 import { useDeck } from '../Deck/useDeck'
 import { useEffect, useState } from 'react'
-export const publicDirPath = location.origin.includes('github') ? '/deck-tarot-react/tarot-art' : '/tarot-art'
+export const publicDirPath = location.origin.includes('github')
+  ? '/deck-tarot-react/tarot-art'
+  : '/tarot-art'
 
 export const useCard = (card) => {
-  const { onCardClick, selectedCardId, lastSelectedCardId, shuffleDeck } = useDeck()
+  const { onCardClick, selectedCardId, lastSelectedCardId, shuffleDeck } =
+    useDeck()
+
+  const preloadImage = (url, callback) => {
+    var img = new Image()
+    img.src = url
+    img.onload = callback
+  }
+
   const cardArtUrl =
     `${publicDirPath}/` +
     (selectedCardId === card.id ? `${card.id}.jpg` : `back.jpg`)
+
   const [flipAnimation, setFlipAnimation] = useState('')
 
   const flip = () => {
@@ -19,14 +30,19 @@ export const useCard = (card) => {
   useEffect(() => {
     const shouldFlip =
       (lastSelectedCardId === card.id || selectedCardId === card.id) &&
-      selectedCardId !== null 
+      selectedCardId !== null
     if (shouldFlip) flip()
-    if(selectedCardId === null) shuffleDeck()
+    if (selectedCardId === null) shuffleDeck()
+
+    // Preload images to avoid animation delay when flipping
+    preloadImage(cardArtUrl, () => { /* console.log('loaded') */ })
   }, [selectedCardId])
 
   const flipAnimationClass = `${
     selectedCardId === card.id ? 'selected' : 'unselected'
-  } animate__animated ${flipAnimation} ${selectedCardId ? '' : 'animate__flipInY '}`
+  } animate__animated ${flipAnimation} ${
+    selectedCardId ? '' : 'animate__flipInY '
+  }`
 
   return { flipAnimationClass, cardArtUrl, onCardClick }
 }
